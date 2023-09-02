3496d43324951065d3d3116684b15c2d67aacb23
Format-qi <jsq15517589670@163.com>
2023-09-02 11:13:04 +0800

    fix: 授权存酒时，输入数量不应该被计算

--- src/components/order/saveWine/drawerSaveWine/drawerChooseWineInfo.vue
+++ src/components/order/saveWine/drawerSaveWine/drawerChooseWineInfo.vue
@@ -210,15 +210,19 @@ export default {
               currentInfo.count = (currentInfo.count == 0 ? '' : currentInfo.count.toString()) + value * 1;
               break;
           }
-          const allCount = this.tableData.reduce((a, b) => a + b.count * 1, 0)
-          if(allCount + this.chooseCount * 1 > this.maxCount * 1) {
-            let anotherCount = 0
-            this.tableData.forEach((el, i) => {
-              if(i != this.focus) {
-                anotherCount += el.count * 1
-              }
-            })
-            currentInfo.count = this.maxCount * 1 - anotherCount - this.chooseCount
+            // 非授权存酒才计算
+          if(this.type == 1){ 
+            const allCount = this.tableData.reduce((a, b) => a + b.count * 1, 0)
+            if(allCount + this.chooseCount * 1 > this.maxCount * 1) {
+              let anotherCount = 0
+              this.tableData.forEach((el, i) => {
+                if(i != this.focus) {
+                  anotherCount += el.count * 1
+                }
+              })
+            
+                currentInfo.count = this.maxCount * 1 - anotherCount - this.chooseCount
+            }
           }
         }
       }
