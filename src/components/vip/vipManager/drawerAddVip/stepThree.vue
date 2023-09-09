6a1a257e0ca8744f9cd4f3e50db289a4e4d6bba7
tanglin <tanglin19811@gmail.com>
2023-09-09 11:52:53 +0800

    feat: 通过姓名过滤

--- src/components/vip/vipManager/drawerAddVip/stepThree.vue
+++ src/components/vip/vipManager/drawerAddVip/stepThree.vue
@@ -7,9 +7,20 @@
           <span>会员卡号:</span>
         </div>
         <div class="value" layout="row" layout-align="start center">
-          <el-input v-model="form.vipNumVal" :maxlength="10" size="small" style="width:284px" placeholder="请输入会员卡号"></el-input>
-          <div v-if="loading" class="loading" layout="row" layout-align="start center">
-            <img :src="require('@/assets/order-img/loading.png')" alt="">
+          <el-input
+            v-model="form.vipNumVal"
+            :maxlength="10"
+            size="small"
+            style="width: 284px"
+            placeholder="请输入会员卡号"
+          ></el-input>
+          <div
+            v-if="loading"
+            class="loading"
+            layout="row"
+            layout-align="start center"
+          >
+            <img :src="require('@/assets/order-img/loading.png')" alt="" />
             <span>自动生成卡号中...</span>
           </div>
         </div>
@@ -20,7 +31,12 @@
           <span>会员卡类别:</span>
         </div>
         <div class="value" layout="row" layout-align="start center">
-          <el-select v-model="form.vipTypeVal" size="small" style="width:284px" placeholder="请选择会员卡类别">
+          <el-select
+            v-model="form.vipTypeVal"
+            size="small"
+            style="width: 284px"
+            placeholder="请选择会员卡类别"
+          >
             <el-option
               v-for="item in form.vipTypeOption"
               :key="item.id"
@@ -36,7 +52,12 @@
           <span>会员卡等级:</span>
         </div>
         <div class="value" layout="row" layout-align="start center">
-          <el-select v-model="form.vipDeepVal" size="small" style="width:284px" placeholder="请选择会员卡等级">
+          <el-select
+            v-model="form.vipDeepVal"
+            size="small"
+            style="width: 284px"
+            placeholder="请选择会员卡等级"
+          >
             <el-option
               v-for="item in form.vipDeepValOption"
               :key="item.id"
@@ -51,7 +72,14 @@
           <span>联系电话:</span>
         </div>
         <div class="value">
-          <el-input type="text" v-model="form.connectPhoneNum" placeholder="请输入联系电话" :maxlength="11"  style="width:284px" size="small" ></el-input>
+          <el-input
+            type="text"
+            v-model="form.connectPhoneNum"
+            placeholder="请输入联系电话"
+            :maxlength="11"
+            style="width: 284px"
+            size="small"
+          ></el-input>
         </div>
       </div>
       <div class="row" layout="row" layout-align="start center">
@@ -59,7 +87,12 @@
           <span>姓名:</span>
         </div>
         <div class="value">
-          <el-input v-model="form.userName" size="small" style="width:284px" placeholder="请输入姓名"></el-input>
+          <el-input
+            v-model="form.userName"
+            size="small"
+            style="width: 284px"
+            placeholder="请输入姓名"
+          ></el-input>
         </div>
       </div>
       <div class="row" layout="row" layout-align="start center">
@@ -83,7 +116,7 @@
             value-format="yyyy-MM-dd"
             placeholder="选择日期"
             :picker-options="pickerOptions"
-            style="width:284px"
+            style="width: 284px"
           ></el-date-picker>
         </div>
       </div>
@@ -92,7 +125,13 @@
           <span>会员卡标签:</span>
         </div>
         <div class="value">
-          <el-radio v-for="item in form.markList" :key="item" v-model="form.markVal" :label="item">{{item}}</el-radio>
+          <el-radio
+            v-for="item in form.markList"
+            :key="item"
+            v-model="form.markVal"
+            :label="item"
+            >{{ item }}</el-radio
+          >
         </div>
       </div>
       <div class="row" layout="row" layout-align="start center">
@@ -100,15 +139,31 @@
           <span>有效期:</span>
         </div>
         <div class="value">
-          <el-radio v-for="item in form.timeLongList" :key="item.id" v-model="form.timeLongVal" :label="item.id">{{item.n}}</el-radio>
+          <el-radio
+            v-for="item in form.timeLongList"
+            :key="item.id"
+            v-model="form.timeLongVal"
+            :label="item.id"
+            >{{ item.n }}</el-radio
+          >
         </div>
       </div>
-      <div v-if="stepOneInfo.radioVal != 2" class="row" layout="row" layout-align="start center">
+      <div
+        v-if="stepOneInfo.radioVal != 2"
+        class="row"
+        layout="row"
+        layout-align="start center"
+      >
         <div class="label">
           <span>订阅短信类型:</span>
         </div>
         <div class="value">
-          <el-checkbox v-model="item.checked" v-for="item in form.messageList" :key="item.id">{{item.n}}</el-checkbox>
+          <el-checkbox
+            v-model="item.checked"
+            v-for="item in form.messageList"
+            :key="item.id"
+            >{{ item.n }}</el-checkbox
+          >
         </div>
       </div>
       <div class="row" layout="row" layout-align="start center">
@@ -117,12 +172,12 @@
         </div>
         <div class="value">
           <el-select
-            style="width:284px"
+            style="width: 284px"
             v-model="form.openCardPersonVal"
             filterable
             remote
             reserve-keyword
-            placeholder="输入员工工号可查询"
+            placeholder="输入员工姓名或工号可查询"
             :remote-method="remoteMethod"
             :loading="remoteLoading"
             size="small"
@@ -130,7 +185,7 @@
             <el-option
               v-for="item in form.openCardPersonOptions"
               :key="item.id"
-              :label="item.name + ' ('+ item.code + ')'"
+              :label="item.name + ' (' + item.code + ')'"
               :value="item.id"
             ></el-option>
           </el-select>
@@ -139,9 +194,9 @@
     </div>
   </div>
 </template>
- 
+
 <script>
-import api_vip from '@/api/vip'
+import api_vip from "@/api/vip";
 export default {
   data() {
     return {
@@ -151,17 +206,17 @@ export default {
         vipTypeOption: [],
         vipDeepVal: "",
         vipDeepValOption: [],
-        connectPhoneNum: '', // 联系电话
+        connectPhoneNum: "", // 联系电话
         userName: "",
         sexVal: "1",
         birthdayVal: "",
         markVal: "会员",
-        markList: [],  // 会员卡标签
+        markList: [], // 会员卡标签
         timeLongVal: 99,
         timeLongList: [],
         messageList: [],
-        openCardPersonVal: '', // 开卡推荐人
-        openCardPersonOptions: []
+        openCardPersonVal: "", // 开卡推荐人
+        openCardPersonOptions: [],
       },
       pickerOptions: {
         disabledDate(time) {
@@ -169,48 +224,52 @@ export default {
         },
       },
       remoteLoading: false,
-      loading: false
+      loading: false,
     };
   },
   methods: {
     // 获取添加会员卡相关option
     async init() {
-      this.getParams()
-      this.getCardNo()
+      this.getParams();
+      this.getCardNo();
     },
-    async getCardNo(){
-      this.loading = true
+    async getCardNo() {
+      this.loading = true;
       try {
-        const res = await api_vip.reqCreateVipCardNo()
+        const res = await api_vip.reqCreateVipCardNo();
         if (res.code == 1) {
           if (!this.form.vipNumVal) {
-            this.form.vipNumVal = res.data.card_no || ''
+            this.form.vipNumVal = res.data.card_no || "";
           }
         } else {
           this.$message.warning(res.msg);
         }
-        this.loading = false
+        this.loading = false;
       } catch (error) {
         console.log("获取可用卡号失败", error);
       }
     },
-    async getParams(){
+    async getParams() {
       try {
-        const res = await api_vip.reqGetAddVipCardParams()
+        const res = await api_vip.reqGetAddVipCardParams();
         if (res.code == 1) {
-          this.form.vipTypeOption = res.data.card_types || []
-          const defaultVipTypeInfo = this.form.vipTypeOption.sort((a, b) => a - b)[0]
-          this.form.vipTypeVal = defaultVipTypeInfo.id
-          this.form.vipDeepValOption = defaultVipTypeInfo.ls || []
-          const defaultVipDeepInfo = this.form.vipDeepValOption.sort((a, b) => a - b)[0]
-          this.form.vipDeepVal = defaultVipDeepInfo.id
+          this.form.vipTypeOption = res.data.card_types || [];
+          const defaultVipTypeInfo = this.form.vipTypeOption.sort(
+            (a, b) => a - b
+          )[0];
+          this.form.vipTypeVal = defaultVipTypeInfo.id;
+          this.form.vipDeepValOption = defaultVipTypeInfo.ls || [];
+          const defaultVipDeepInfo = this.form.vipDeepValOption.sort(
+            (a, b) => a - b
+          )[0];
+          this.form.vipDeepVal = defaultVipDeepInfo.id;
 
-          this.form.markList = res.data.marks || []
-          this.form.timeLongList = res.data.valid_years || []
-          this.form.messageList = (res.data.sms_types || []).map(item => ({
+          this.form.markList = res.data.marks || [];
+          this.form.timeLongList = res.data.valid_years || [];
+          this.form.messageList = (res.data.sms_types || []).map((item) => ({
             ...item,
-            checked: false
-          }))
+            checked: false,
+          }));
         } else {
           this.$message.warning(res.msg);
         }
@@ -221,44 +280,52 @@ export default {
     remoteMethod(query) {
       if (query !== "") {
         this.remoteLoading = true;
-        this.form.openCardPersonOptions = JSON.parse(JSON.stringify(this.openCardPersonOptions)).filter(item => item.code.includes(query))
-        this.remoteLoading = false
+        this.form.openCardPersonOptions = JSON.parse(
+          JSON.stringify(this.openCardPersonOptions)
+        ).filter(
+          (item) => item.code.includes(query) || item.name.includes(query)
+        );
+        this.remoteLoading = false;
       } else {
         this.options = [...this.openCardPersonOptions];
       }
-    }
+    },
   },
   created() {
-    this.init()
+    this.init();
   },
   mounted() {},
-  props:{
+  props: {
     stepOneInfo: {
-      default: {}
-    }
+      default: {},
+    },
   },
   computed: {
     openCardPersonOptions() {
-      const result = this.$store.state.cardPageInfo.resResultDataObj.orderPersonInfo || []
-      this.form.openCardPersonOptions = [...result]
-      return result
-    }
+      const result =
+        this.$store.state.cardPageInfo.resResultDataObj.orderPersonInfo || [];
+      this.form.openCardPersonOptions = [...result];
+      return result;
+    },
   },
   watch: {
-    'form.vipTypeVal'(newVal){
-      const defaultVipTypeInfo = this.form.vipTypeOption.find(item => item.id == newVal) || {}
-      this.form.vipDeepValOption = defaultVipTypeInfo.ls || []
-      const defaultVipDeepInfo = this.form.vipDeepValOption.sort((a, b) => a - b)[0]
-      this.form.vipDeepVal = defaultVipDeepInfo.id
+    "form.vipTypeVal"(newVal) {
+      const defaultVipTypeInfo =
+        this.form.vipTypeOption.find((item) => item.id == newVal) || {};
+      this.form.vipDeepValOption = defaultVipTypeInfo.ls || [];
+      const defaultVipDeepInfo = this.form.vipDeepValOption.sort(
+        (a, b) => a - b
+      )[0];
+      this.form.vipDeepVal = defaultVipDeepInfo.id;
     },
     form: {
       handler(newVal) {
-        this.$emit('updateStepThreeInfo', newVal)
+        this.$emit("updateStepThreeInfo", newVal);
       },
       deep: true,
-      immediate: true
-    }
-  }
+      immediate: true,
+    },
+  },
 };
 </script>
 
@@ -268,7 +335,7 @@ export default {
 </style>
 
 <style>
-.el-date-table td.disabled div{
+.el-date-table td.disabled div {
   background-color: transparent;
   color: #999;
 }
