7dcc05b1a2f258cf6be8044ebf6e0bb7638b7d86
tanglin <tanglin19811@gmail.com>
2023-09-17 20:58:48 +0800

    feat: 修改参数类型

--- src/components/vip/vipManager/drawerOptionEvent/index.vue
+++ src/components/vip/vipManager/drawerOptionEvent/index.vue
@@ -264,7 +264,7 @@ export default {
         sms_type_ids: this.updateVipInfoObj.messageList
           .filter((item) => item.checked)
           .map((item) => item.id * 1), // []int        //SmsTypeIds 订阅消息列表
-        sales_emp_id: this.updateVipInfoObj.personVal, //
+        sales_emp_id: this.updateVipInfoObj.personVal * 1, //
       };
       try {
         const res = await api_vip.reqUpdateVipCard(params);
