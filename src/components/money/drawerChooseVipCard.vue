057556dd48556cc1bfcff44c226c48369288a3b0
tanglin <tanglin19811@gmail.com>
2023-09-23 19:15:16 +0800

    feat: add

--- src/components/money/drawerChooseVipCard.vue
+++ src/components/money/drawerChooseVipCard.vue
@@ -18,7 +18,11 @@
                 <span>付款码序列号:</span>
               </div>
               <div class="value" layout="row" layout-align="start center">
-                <input type="text" v-model="validateVal" placeholder="请输入付款码序列号" />
+                <input
+                  type="text"
+                  v-model="validateVal"
+                  placeholder="请输入付款码序列号"
+                />
                 <div class="btn" @click="getTableData">搜索</div>
               </div>
             </div>
@@ -30,8 +34,18 @@
                 <span>手机号:</span>
               </div>
               <div class="value" layout="row" layout-align="start center">
-                <input type="text" v-model="phoneNumVal" placeholder="请输入手机号" />
-                <div class="btn" :class="{disabled: count<60}" @click="sendPhoneMessage">{{btnText}}</div>
+                <input
+                  type="text"
+                  v-model="phoneNumVal"
+                  placeholder="请输入手机号"
+                />
+                <div
+                  class="btn"
+                  :class="{ disabled: count < 60 }"
+                  @click="sendPhoneMessage"
+                >
+                  {{ btnText }}
+                </div>
               </div>
             </div>
             <div class="row" layout="row" layout-align="start center">
@@ -40,14 +54,18 @@
                 <span>验证码:</span>
               </div>
               <div class="value" layout="row" layout-align="start center">
-                <input type="text" v-model="validateVal" placeholder="请输入验证码" />
+                <input
+                  type="text"
+                  v-model="validateVal"
+                  placeholder="请输入验证码"
+                />
                 <div class="btn" @click="getTableData">搜索</div>
               </div>
             </div>
           </template>
         </div>
 
-        <p class="tips red">提示：勾选会员卡后，方可填写使用金额</p>
+        <p class="tips red">提示：勾选会员卡后，才可以填写金额</p>
 
         <div class="table">
           <div class="thead">
@@ -56,7 +74,10 @@
               <div class="th">会员卡号</div>
               <div class="th">会员名称</div>
               <div class="th">会员卡金额</div>
-              <div class="th">使用金额</div>
+              <div class="th">卡可用余额</div>
+              <div class="th">本次使用金额</div>
+              <div class="th">本次使用储值金额</div>
+              <div class="th">本次使用赠送金额</div>
             </div>
           </div>
           <div class="tbody">
@@ -72,11 +93,13 @@
                   v-model="item.checked"
                   :disabled="item.disabled"
                   @change="changeCheckBox(item)"
-                >{{index + 1}}</el-checkbox>
+                  >{{ index + 1 }}</el-checkbox
+                >
               </div>
-              <div class="td one-txt-cut">{{item.c}}</div>
-              <div class="td one-txt-cut">{{item.n}}</div>
-              <div class="td one-txt-cut">{{item.b}}</div>
+              <div class="td one-txt-cut">{{ item.c }}</div>
+              <div class="td one-txt-cut">{{ item.n }}</div>
+              <div class="td one-txt-cut">{{ item.b }}</div>
+              <div class="td one-txt-cut">{{ item.b }}</div>
               <div class="td one-txt-cut">
                 <input
                   type="text"
@@ -86,8 +109,16 @@
                   @input="changeInput(item, item.b)"
                 />
               </div>
+              <div class="td one-txt-cut">{{ item.b }}</div>
+              <div class="td one-txt-cut">{{ item.b }}</div>
             </div>
-            <p v-if="tableData.length == 0" class="m-t-10 fs14" style="text-align:center">暂无数据</p>
+            <p
+              v-if="tableData.length == 0"
+              class="m-t-10 fs14"
+              style="text-align: center"
+            >
+              暂无数据
+            </p>
           </div>
         </div>
       </div>
@@ -98,7 +129,7 @@
     </el-drawer>
   </div>
 </template>
- 
+
 <script>
 import api_money from "@/api/money";
 import api_vip from "@/api/vip";
@@ -111,7 +142,7 @@ export default {
       phoneNumVal: "",
       validateVal: "",
       authCodeStr: "",
-      tableData: []
+      tableData: [],
     };
   },
   methods: {
@@ -126,11 +157,15 @@ export default {
       const storageSecondCount = this.$sessionStorage.getItem(
         "secondCountFormChooseVip"
       ); // 获取发送短信时的时间戳
-      const now = +new Date()
-      const oneMinute = +new Date('2023/05/25 12:01:00') - +new Date('2023/05/25 12:00:00')
+      const now = +new Date();
+      const oneMinute =
+        +new Date("2023/05/25 12:01:00") - +new Date("2023/05/25 12:00:00");
       if (now - storageSecondCount < oneMinute) {
         // 倒计时为结束接着倒计时
-        this.count = ((oneMinute - (now - storageSecondCount)) / (oneMinute / 60)).toFixed(0) * 1;
+        this.count =
+          ((oneMinute - (now - storageSecondCount)) / (oneMinute / 60)).toFixed(
+            0
+          ) * 1;
         this.loopSecond();
       } else {
         this.count = 60;
@@ -141,7 +176,7 @@ export default {
       if (this.count != 60) return;
       const params = {
         t: 200, //  int   200  验证获取客人会员卡结账
-        m: this.phoneNumVal //  string   手机号
+        m: this.phoneNumVal, //  string   手机号
       };
       if (params.m.length != 11)
         return this.$message.warning("请输入正确的11位手机号");
@@ -166,7 +201,7 @@ export default {
     async validatePhoneInfo() {
       const params = {
         m: this.phoneNumVal, //   string    手机号
-        c: this.validateVal //    string   验证码
+        c: this.validateVal, //    string   验证码
       };
 
       if (params.c.length != 5) {
@@ -176,7 +211,7 @@ export default {
       try {
         const res = await api_vip.reqValidatePhoneMsg(params);
         if (res.code == 1) {
-          this.authCodeStr = res.data.c
+          this.authCodeStr = res.data.c;
           return res.data.c;
         } else {
           this.$message.warning(res.msg);
@@ -186,9 +221,9 @@ export default {
       }
     },
 
-    async validateValidateInfo(){
+    async validateValidateInfo() {
       const params = {
-        pay_code: this.validateVal //    string   付款码
+        pay_code: this.validateVal, //    string   付款码
       };
       if (params.pay_code.length != 6) {
         this.$message.warning("请输入正确的六位付款码序列号");
@@ -197,8 +232,8 @@ export default {
       try {
         const res = await api_money.reqGetVipCardFormValidate(params);
         if (res.code == 1) {
-          this.phoneNumVal = res.data.p
-          this.authCodeStr = res.data.c
+          this.phoneNumVal = res.data.p;
+          this.authCodeStr = res.data.c;
           return res.data.c;
         } else {
           this.$message.warning(res.msg);
@@ -221,27 +256,30 @@ export default {
     },
 
     async getTableData() {
-      const result = this.type == 1 ? await this.validateValidateInfo() : await this.validatePhoneInfo();
+      const result =
+        this.type == 1
+          ? await this.validateValidateInfo()
+          : await this.validatePhoneInfo();
       if (result) {
         const params = {
           sms_auth_code: result, // string   手机验证码验证授权串
-          phone_num: this.phoneNumVal //  string   手机号
+          phone_num: this.phoneNumVal, //  string   手机号
         };
 
         try {
           const res = await api_money.reqGetVipCardFormPhoneNum(params);
           if (res.code == 1) {
-            this.tableData = (res.data.records || []).map(item => ({
+            this.tableData = (res.data.records || []).map((item) => ({
               ...item,
               checked: (res.data.records || []).length == 1,
               chooseAmt: "",
-              disabled: false
-            }))
-            if(this.tableData.length == 1) {
-              this.changeCheckBox(this.tableData[0])
+              disabled: false,
+            }));
+            if (this.tableData.length == 1) {
+              this.changeCheckBox(this.tableData[0]);
             }
           } else {
-            this.$message.warning(res.msg)
+            this.$message.warning(res.msg);
           }
         } catch (error) {
           console.log("通过手机号查询会员卡失败", error);
@@ -257,24 +295,24 @@ export default {
           this.maxPayMoney * 1
         ) {
           itemInfo.chooseAmt = itemInfo.b * 1;
-          this.tableData = this.tableData.map(item => ({
+          this.tableData = this.tableData.map((item) => ({
             ...item,
-            disabled: false
+            disabled: false,
           }));
         } else {
           itemInfo.chooseAmt = (
             (this.maxPayMoney - choosedAmt).toFixed(2) * 1
           ).toString();
-          this.tableData = this.tableData.map(item => ({
+          this.tableData = this.tableData.map((item) => ({
             ...item,
-            disabled: !item.checked
+            disabled: !item.checked,
           }));
         }
       } else {
         itemInfo.chooseAmt = "";
-        this.tableData = this.tableData.map(item => ({
+        this.tableData = this.tableData.map((item) => ({
           ...item,
-          disabled: false
+          disabled: false,
         }));
         this.$forceUpdate();
       }
@@ -282,7 +320,7 @@ export default {
 
     changeInput(itemInfo, max) {
       const choosedAmt = this.tableData
-        .filter(item => item.checked && item.id != itemInfo.id)
+        .filter((item) => item.checked && item.id != itemInfo.id)
         .reduce((a, b) => a + b.chooseAmt * 1, 0);
       if (isNaN(itemInfo.chooseAmt)) {
         this.$message.warning("请输入数字");
@@ -305,9 +343,9 @@ export default {
       }
 
       if (this.choosedAmt * 1 < this.maxPayMoney * 1) {
-        this.tableData = this.tableData.map(item => ({
+        this.tableData = this.tableData.map((item) => ({
           ...item,
-          disabled: false
+          disabled: false,
         }));
       }
     },
@@ -317,12 +355,18 @@ export default {
         seat_id: this.$store.state.orderInfo.currentCardInfo.seatId * 1, //    int64    待操作卡台Id
         auth_type: 2, //  int       授权类型 1 卡授权 2 手机验证码授权
         auth_code: this.authCodeStr, //  string    授权串
-        mb_card_ids: this.tableData.filter(item => item.checked).map(item => item.id * 1), // []int64    会员卡Id列表
-        amts: this.tableData.filter(item => item.checked).map(item => item.chooseAmt.toString()), //       []string   使用金额
-        ...this.payId && { pay_id: this.payId * 1 }
+        mb_card_ids: this.tableData
+          .filter((item) => item.checked)
+          .map((item) => item.id * 1), // []int64    会员卡Id列表
+        amts: this.tableData
+          .filter((item) => item.checked)
+          .map((item) => item.chooseAmt.toString()), //       []string   使用金额
+        ...(this.payId && { pay_id: this.payId * 1 }),
       };
       try {
-        const api = this.payId ? 'reqAddVipCardFormPhoneNumInPayAfter' : 'reqAddVipCardFormPhoneNumInPayBefore'
+        const api = this.payId
+          ? "reqAddVipCardFormPhoneNumInPayAfter"
+          : "reqAddVipCardFormPhoneNumInPayBefore";
         const res = await api_money[api](params);
         if (res.code == 1) {
           this.$message.success("添加成功");
@@ -337,22 +381,22 @@ export default {
     },
     onCancelDrawer() {
       this.$emit("showOrHideDrawer");
-    }
+    },
   },
   mounted() {},
   props: {
     showDrawer: {
-      default: false // 是否显示drawer
+      default: false, // 是否显示drawer
     },
     maxPayMoney: {
-      default: "0"
+      default: "0",
     },
     type: {
-      default: 1   // 1:二维码序列号查找  2:手机号查找
+      default: 1, // 1:二维码序列号查找  2:手机号查找
     },
     payId: {
-      default: ''
-    }
+      default: "",
+    },
   },
   computed: {
     btnText() {
@@ -362,11 +406,11 @@ export default {
     choosedAmt() {
       const result =
         this.tableData
-          .filter(item => item.checked)
+          .filter((item) => item.checked)
           .reduce((a, b) => a + b.chooseAmt * 1, 0)
           .toFixed(2) * 1;
       return result;
-    }
+    },
   },
   watch: {
     showDrawer: {
@@ -376,9 +420,9 @@ export default {
           this.init();
         }
       },
-      immediate: true
-    }
-  }
+      immediate: true,
+    },
+  },
 };
 </script>
 
@@ -393,7 +437,7 @@ export default {
   padding: 20px;
   color: rgba(255, 255, 255, 8);
   font-size: 14px;
-  
+
   .red {
     color: #ff2f64;
   }
@@ -433,17 +477,17 @@ export default {
     }
   }
 
-  .tips{
+  .tips {
     line-height: 60px;
   }
 
   .table {
-    .thead .tr{
+    .thead .tr {
       background-color: rgba(255, 255, 255, 0.04);
     }
     .tr {
       height: 40px;
-      &:nth-child(2n){
+      &:nth-child(2n) {
         background-color: rgba(255, 255, 255, 0.04);
       }
     }
