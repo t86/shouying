2cc18f8da37cee8ae9633e7ac587638973a6bd64
tanglin <tanglin19811@gmail.com>
2023-11-01 23:07:49 +0800

    类型不对

--- src/views/ManagementSystem/BMS/printers/drawerAddOrUpdatePrinters.vue
+++ src/views/ManagementSystem/BMS/printers/drawerAddOrUpdatePrinters.vue
@@ -109,7 +109,7 @@ export default {
           this.name = res.data.name;
           this.ip = res.data.ip_addr;
           this.prtType = res.data.type_id; // 1 普通打印 2 标签打印
-          this.monopoly = res.data.exclude_mode;
+          this.monopoly = res.data.exclude_mode == 1 ? true : false;
         } else {
           this.$message.warning(res.msg);
         }
