06018b9ff757777813e457aaa1710fd7b52a2a7e
tanglin <tanglin19811@gmail.com>
2023-11-01 22:26:09 +0800

    fix

--- src/components/order/drawerMeal/singleProduct.vue
+++ src/components/order/drawerMeal/singleProduct.vue
@@ -527,6 +527,10 @@ export default {
       if (this.productInfo.canSealYH2) {
         status.push(5);
       }
+      // 这是认为只配置了特饮,设置花篮
+      if(status.length == 0) {
+        status.push(3);
+      }
       return status;
     },
 
