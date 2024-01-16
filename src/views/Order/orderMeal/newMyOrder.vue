f7d25bc255570c160c915ede28035a166e869fed
tanglin <tanglin19811@hotmai.com>
2024-01-16 14:24:42 +0800

    购物车套餐子明细颜色

--- src/views/Order/orderMeal/newMyOrder.vue
+++ src/views/Order/orderMeal/newMyOrder.vue
@@ -91,7 +91,7 @@
               <div v-if="item.is == 1 && !item.back">
                 <div class="detail-list tr" v-for="(items, i) in item.si" :key="i">
                   <div class="td"></div>
-                  <div class="td one-txt-cut">
+                  <div class="td one-txt-cut detail">
                     {{ items.groupInfo.name
                     }}{{ items.r ? "（" + items.r + "）" : "" }}
                   </div>
