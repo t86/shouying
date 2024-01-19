f5d0098cb91d88dc7a2574e5527d560cb67e88e9
tanglin <tanglin19811@hotmai.com>
2024-01-19 15:58:25 +0800

    隐藏右侧操作菜单

--- src/views/Order/orderMeal/newMyOrder.vue
+++ src/views/Order/orderMeal/newMyOrder.vue
@@ -60,7 +60,8 @@
                 <div class="td">
                   <!-- <div class="bg" v-if="item.showList" @click="showOrHideList(item)"></div> -->
                   <img v-if="
-                    (item.s != 5 && !item.back) || item.productInfo.prdType == 2
+                    ((item.s != 5 && !item.back) || item.productInfo.prdType == 2)
+                    && (hasOrderBackAuth || hasChangeDetailAuth)
                   " :src="imgSrc.shoppingCarMore" @click.stop="showOrHideList(item)" alt />
                   <img :src="imgSrc.sanJiao" v-if="
                     item.showList &&
