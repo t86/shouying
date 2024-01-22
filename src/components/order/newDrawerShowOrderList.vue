3e672eb090c11e99b2558f923301010426854999
tanglin <tanglin19811@hotmai.com>
2024-01-22 17:16:49 +0800

    数量字体调整

--- src/components/order/newDrawerShowOrderList.vue
+++ src/components/order/newDrawerShowOrderList.vue
@@ -88,7 +88,7 @@
                             <div
                               class="online-detail-td one-txt-cut name"
                             >{{itemss.groupInfo.name}}{{itemss.r ? '（'+itemss.r+'）':''}}</div>
-                            <div class="online-detail-td">{{itemss.sc * itemss.dpc * items.pc}}</div>
+                            <div class="online-detail-td" style="font-size: 12px">{{itemss.sc * itemss.dpc * items.pc}}</div>
                             <div class="online-detail-td"></div>
                             <div class="online-detail-td"></div>
                             <div class="online-detail-td"></div>
@@ -137,7 +137,7 @@
                       <div
                         class="td one-txt-cut name"
                       >{{items.groupInfo.name}}{{items.r ? '（'+items.r+'）':''}}</div>
-                      <div class="td">{{items.sc * items.dpc * item.pc}}</div>
+                      <div class="td" style="font-size: 12px">{{items.sc * items.dpc * item.pc}}</div>
                       <div class="td"></div>
                       <div class="td"></div>
                       <div class="td"></div>
