0218d7d710c6f83c6256886431043263cc17a88b
tanglin <tanglin19811@gmail.com>
2024-08-07 09:02:29 +0800

    有效期加时间

--- src/views/ManagementSystem/wine/saveNeedToLib/saveNeedToLib.vue
+++ src/views/ManagementSystem/wine/saveNeedToLib/saveNeedToLib.vue
@@ -78,7 +78,8 @@
                   <div class="tr-td">{{items.u}}</div>
                   <div class="tr-td">{{items.g || '-'}}</div>
                   <div class="tr-td">{{items.c}}</div>
-                  <div class="tr-td">{{items.e}}</div>
+                  <div class="tr-td"><span>{{items.e}}</span>
+                    <span :style="{'color': items.ed!=items.ce ? 'red': ''}">({{ items.ed }}天)</span></div>
                   <div class="tr-td">{{items.a}}</div>
                 </div>
               </div>
