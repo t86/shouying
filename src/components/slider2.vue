b1fe4c0370b273272c6ccd45edb8881a7db2718a
trump.liu <kakaliush@push-tech.com>
2024-08-08 16:28:48 +0800

    update slide

--- src/components/slider2.vue
+++ src/components/slider2.vue
@@ -127,77 +127,77 @@ export default {
 </script>
 
 <style scoped lang="less">
-@import "../style/common/newElementDrawer.less";
-@import "../style/common/elementDrawerHeaderAndSession.less";
-@import "../style/common/newElementFormBtn.less";
+//@import "../style/common/newElementDrawer.less";
+//@import "../style/common/elementDrawerHeaderAndSession.less";
+//@import "../style/common/newElementFormBtn.less";
 </style>
 
 <style lang="less" scoped>
-/deep/ label {
-  width: 80px !important;
-  color: rgba(255, 255, 255, 0.8);
-}
-/deep/ .el-radio-button__inner, .el-radio-group {
-  line-height: 1;
-  vertical-align: middle;
-  display: block;
-  //line-height: 1;
-  //vertical-align: middle;
-  //display: flex;
-  //justify-content: center;
-}
-/deep/ .el-radio-button__inner {
-  margin-top: 10px;
-  white-space: nowrap;
-  background: #FFF;
-  border: 1px solid #DCDFE6;
-  font-weight: 500;
-  border-left: 0;
-  color: #606266;
-  -webkit-appearance: none;
-  text-align: center;
-  box-sizing: border-box;
-  outline: 0;
-  margin: 0;
-  position: relative;
-  cursor: pointer;
-  transition: all .3s cubic-bezier(.645,.045,.355,1);
-  padding: 12px 20px;
-  font-size: 20px;
-  border-radius: 0;
-}
-
-/deep/ .el-input--small {
-  font-size: 20px;
-  top: 5px;
-}
-
-/deep/ .el-input-number__decrease {
-  width: 40px;
-  height: 38px;
-  font-size: 30px;
-  font-weight: bold;
-  left: -20px;
-  border-radius: 4px 0 0 4px;
-  border-right: 1px solid #DCDFE6;
-}
-
-/deep/ .el-input-number__increase {
-  width: 30px;
-  height: 38px;
-  font-size: 30px;
-  font-weight: bold;
-  right: -20px;
-  border-radius: 0 4px 4px 0;
-  border-left: 1px solid #DCDFE6;
-}
-/deep/ .el-input__inner {
-  color: rgba(255, 255, 255, 0.8);
-  background: #409EFF;
-  border-radius: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.15);
-  resize: none;
-  /* margin-top: 5px; */
-  //height: 40px;
-}
+///deep/ label {
+//  width: 80px !important;
+//  color: rgba(255, 255, 255, 0.8);
+//}
+///deep/ .el-radio-button__inner, .el-radio-group {
+//  line-height: 1;
+//  vertical-align: middle;
+//  display: block;
+//  //line-height: 1;
+//  //vertical-align: middle;
+//  //display: flex;
+//  //justify-content: center;
+//}
+///deep/ .el-radio-button__inner {
+//  margin-top: 10px;
+//  white-space: nowrap;
+//  background: #FFF;
+//  border: 1px solid #DCDFE6;
+//  font-weight: 500;
+//  border-left: 0;
+//  color: #606266;
+//  -webkit-appearance: none;
+//  text-align: center;
+//  box-sizing: border-box;
+//  outline: 0;
+//  margin: 0;
+//  position: relative;
+//  cursor: pointer;
+//  transition: all .3s cubic-bezier(.645,.045,.355,1);
+//  padding: 12px 20px;
+//  font-size: 20px;
+//  border-radius: 0;
+//}
+//
+///deep/ .el-input--small {
+//  font-size: 20px;
+//  top: 5px;
+//}
+//
+///deep/ .el-input-number__decrease {
+//  width: 40px;
+//  height: 38px;
+//  font-size: 30px;
+//  font-weight: bold;
+//  left: -20px;
+//  border-radius: 4px 0 0 4px;
+//  border-right: 1px solid #DCDFE6;
+//}
+//
+///deep/ .el-input-number__increase {
+//  width: 30px;
+//  height: 38px;
+//  font-size: 30px;
+//  font-weight: bold;
+//  right: -20px;
+//  border-radius: 0 4px 4px 0;
+//  border-left: 1px solid #DCDFE6;
+//}
+///deep/ .el-input__inner {
+//  color: rgba(255, 255, 255, 0.8);
+//  background: #409EFF;
+//  border-radius: 10px;
+//  border: 1px solid rgba(255, 255, 255, 0.15);
+//  resize: none;
+//  /* margin-top: 5px; */
+//  //height: 40px;
+//}
 </style>
