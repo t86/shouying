5366f7a3676a2a3fc462b0091a728bc69a6235c6
trumpliu <kakaliush@163.com>
2024-09-07 23:28:22 +0800

    点单选中间距

--- src/components/money/notPayOrder.vue
+++ src/components/money/notPayOrder.vue
@@ -5,10 +5,10 @@
       <div class="thead">
         <div class="tr">
           <div class="th" layout="row" layout-align="start center">
-            <input type="checkbox" v-model="checkoutAll" @change="chooseOrder($event, '', 'all')" />
+            <input type="checkbox" class="w-12" v-model="checkoutAll" @change="chooseOrder($event, '', 'all')" />
             <span style="white-space:nowrap">全选</span>
           </div>
-          <div class="th">状态</div>
+          <div class="th w-30">状态</div>
           <div class="th">名称</div>
           <div class="th">数量</div>
           <div class="th">待结数量</div>
@@ -33,7 +33,7 @@
                 <div class="td" layout="row" layout-align="start center">
                   <input
                     type="checkbox"
-                    class="m-r-1"
+                    class="m-r-1 w-12"
                     :disabled="item.resultNotPayData.every(items=>items.back)"
                     v-model="item.checkout"
                     @change="chooseOrder($event,item,'item')"
@@ -157,7 +157,7 @@
                 <div class="td" layout="row" layout-align="start center">
                   <input
                     type="checkbox"
-                    class="m-r-1"
+                    class="m-r-1 w-12"
                     :disabled="item.back"
                     v-model="item.checkout"
                     @change="chooseOrder($event, item, 'item')"
