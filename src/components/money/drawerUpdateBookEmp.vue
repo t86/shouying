29801f79319caeb6d7422086ccff2a739ff40aa7
trump.liu <kakaliush@push-tech.com>
2024-09-13 14:58:32 +0800

    修改订位人记录

--- src/components/money/drawerUpdateBookEmp.vue
+++ src/components/money/drawerUpdateBookEmp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="turn-over-detail">
     <!-- 修改订位人记录 -->
     <el-drawer
       title="修改订位人记录"
@@ -8,7 +8,7 @@
       direction="rtl"
       size="98%"
     >
-      <div class="turn-over-detail">
+      <div >
         <div class="top" layout="row" layout-align="space-between center">
           <!-- <div class="top-left" layout="row" layout-align="start center">
             <div class="coll" layout="row" layout-align="start center">
