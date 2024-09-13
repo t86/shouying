6fb4fb418cbd204e41a300eea899384459f22f0f
trump.liu <kakaliush@push-tech.com>
2024-09-13 14:46:47 +0800

    翻台记录

--- src/components/money/drawerTurnOver.vue
+++ src/components/money/drawerTurnOver.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="turn-over-detail">
     <!-- 翻台记录 -->
     <el-drawer
       title="翻台记录"
@@ -8,7 +8,7 @@
       direction="rtl"
       size="98%"
     >
-      <div class="turn-over-detail">
+      <div>
         <div class="top" layout="row" layout-align="space-between center">
           <!-- <div class="top-left" layout="row" layout-align="start center">
             <div class="coll" layout="row" layout-align="start center">
