c8fe8c2cea3001cfcbcfb8fef93113c885be4856
trump.liu <trump.liu@live.com>
2025-04-07 14:22:03 +0800

    修复保存商户号

--- src/components/money/drawerMerchant.vue
+++ src/components/money/drawerMerchant.vue
@@ -303,6 +303,78 @@ import api_money from "@/api/money";
 let loaded = false // 数据是否加载完成
 let updated = false  // 数据是否被修改
 const leftTableData = [
+{
+    id: 0,
+    name: '00:00-01:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 1,
+    name: '01:00-02:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 2,
+    name: '02:00-03:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 3,
+    name: '03:00-04:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 4,
+    name: '04:00-05:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 5,
+    name: '05:00-06:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 6,
+    name: '06:00-07:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 7,
+    name: '07:00-08:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 8,
+    name: '08:00-09:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 9,
+    name: '09:00-10:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 10,
+    name: '10:00-11:00',
+    amt: '0',
+    checkedId: ''
+  },
+  {
+    id: 11,
+    name: '11:00-12:00',
+    amt: '0',
+    checkedId: ''
+  },
     {
     id: 12,
     name: '12:00-13:00',
