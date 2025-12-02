5663911d76788549800724ee40aca20ac7972e29
tanglin <tanglin19811@gmail.com>
2025-12-02 09:52:17 +0800

    区域营收增加开台数

---
+++ src/api/BMS/emp/businessEmp.js
@@ -0,0 +1,23 @@
+import base from '../../base.js';
+import axios from '../../../utils/http.js';
+
+const businessEmp = {
+  // 获取商务组订位人列表
+  reqGetBsSalesList(data) {
+    return axios.post(`${base.htgl}/emp/get_bs_sales_list`, data);
+  },
+
+  // 批量添加商务组订位人
+  reqBatchAddBsSales(data) {
+    return axios.post(`${base.htgl}/emp/batch_add_bs_sales`, data);
+  },
+
+  // 删除商务组订位人
+  reqRemoveBsSales(data) {
+    return axios.post(`${base.htgl}/emp/remove_bs_sales`, data);
+  },
+};
+
+export default businessEmp;
+
+
